6e24849a8e8babf42e2368e55e44f5ecceaf8edf
Eva Antipina <eva.antipina@gmail.com>
2018-04-06 12:43:39 -0700

    Add working and tested bussiness logic.

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// Business logic.
+
+function Stuff() {
+  this.choices = [];
+}
+
+//var veggies = new Stuff();
+//var meet = new Stuff();
+
+function Pizza(size, crust) {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = [];
+  this.cheese = new Stuff;
+}
+
+// Prototype function tocalculate pizza cost.
+Pizza.prototype.cost = function() {
+  var price = parseInt(this.size) + this.toppings.length + parseInt(this.crust);
+  for(var i = 0; i < this.cheese.length; i++) {
+    price += 0.30;
+  }
+  return price;
+}
+
+// UI logic.
